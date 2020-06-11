87ff7e0e782f2e26592b5baae7fa5f79fd5dd96a
ccsantelises <66543832+ccsantelises@users.noreply.github.com>
2020-06-11 10:56:00 -0400

    Added file

---
+++ guessinggame.sh
@@ -0,0 +1,31 @@
+echo "Hello! Welcome to the Guessing game!"
+
+function ask {
+	echo "Please enter the number of files you think are in the current directory:"
+	read guess
+    files=$(ls -1 | wc -l)
+}
+
+ask
+
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+		echo "You entered $guess. Your guess was too low, keep guessing."
+	else
+		echo "You entered $guess. Your guess was too high, keep guessing."
+	fi
+	ask
+done
+
+echo "Congratulations! You guessed right, woohoo!"
+echo "Well done."
+
+read -p "Would you like to see the list of files in the current directory? (y/n)" choice
+echo #
+if [ "$choice" = "y" ]; then
+  echo "---" && ls -1;
+else
+  echo "Exit Game";
+fi
